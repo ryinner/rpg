78201cfc6a52bc0cbb7d9382b1965cc9fa1f5e3f
cherepanovyan@gmail.com <cherepanovyan@gmail.com>
2025-01-28 21:54:39 +0500

    path A* without collider

--- Assets/Scripts/Pathfinding/Testing.cs
+++ Assets/Scripts/Pathfinding/Testing.cs
@@ -16,34 +16,31 @@ public class Testing : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 position = GetMouseWorldPosition();
-            pathfinding.GetGrid().GetXZ(position, out int x, out int z);
+            Grid<PathNode> grid = pathfinding.GetGrid();
+            grid.GetXZ(position, out int x, out int z);
             List<PathNode> path = pathfinding.FindPath(0, 0, x, z);
             if (path != null)
-            {
-                for (int i = 0; i < path.Count - 1; i++)
-                {
-                    Debug.DrawLine(new Vector3(path[i].x, 0, path[i].z) * 10f + Vector3.one * 5f, new Vector3(path[i + 1].x, 0, path[i + 1].z) * 10f + Vector3.one * 5f, Color.green, 5f);
+                for (int i = 0; i < path.Count - 1; i++){
+                 
+                 if(grid.GetShowDebug()) Debug.DrawLine(new Vector3(path[i].x, 0, path[i].z) * grid.GetCellSize() + Vector3.one * grid.GetCellSize() * .5f, new Vector3(path[i + 1].x, 0, path[i + 1].z) * grid.GetCellSize() + Vector3.one * grid.GetCellSize() * .5f, Color.green, 1f);
                 }
-            }
         }
 
         if (Input.GetMouseButtonDown(1))
         {
             Vector3 position = GetMouseWorldPosition();
             pathfinding.GetGrid().GetXZ(position, out int x, out int z);
-            
+
             PathNode pathNode = pathfinding.GetNode(x, z);
-            if(pathNode != null) pathNode.isWalkable = !pathNode.isWalkable;
+            if (pathNode != null) pathNode.isWalkable = !pathNode.isWalkable;
         }
     }
 
     private Vector3 GetMouseWorldPosition()
     {
-
-        float distance;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Vector3 worldPosition = Vector3.zero;
-        if (new Plane(Vector3.up, 0).Raycast(ray, out distance))
+        if (new Plane(Vector3.up, 0).Raycast(ray,  out float distance))
         {
             worldPosition = ray.GetPoint(distance);
         }
